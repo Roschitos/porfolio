91c8d9b24fd0ef2b8f98e80dd06ec3b126180c67
Roschitos <112013548+Roschitos@users.noreply.github.com>
2025-03-17 20:40:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,29 +14,32 @@ document.addEventListener("DOMContentLoaded", () => {
     fadeInElements.forEach(el => observer.observe(el));
 
     // Cookie banner
-    const cookieBanner = document.createElement("div");
-    cookieBanner.id = "cookie-banner";
-    cookieBanner.innerHTML = `<div class="cookie-content">
-            <p>Questo sito si serve dei cookies per garantire la miglior esperienza possibile.</p>
-            <p>Premendo "Accetta" acconsenti all'uso dei cookies.</p>
-            <p>Per maggiori informazioni, per favore leggere l'<a href="https://www.garanteprivacy.it/temi/informativa", target="_blank">Informativa</a>.</p>
-            <button id="accept-cookies">Accetta</button> <button id="rejected-cookies">Continua senza</button>
-        </div>`;
-    document.body.appendChild(cookieBanner);
+    if (!localStorage.getItem("cookiesAccepted") && !localStorage.getItem("cookiesRejected")) {
+        const cookieBanner = document.createElement("div");
+        cookieBanner.id = "cookie-banner";
+        cookieBanner.innerHTML = `
+            <div class="cookie-content">
+                <p>Questo sito si serve dei cookies per garantire la miglior esperienza possibile.</p>
+                <p>Premendo "Accetta" acconsenti all'uso dei cookies.</p>
+                <p>Per maggiori informazioni, leggere l'<a href="https://www.garanteprivacy.it/temi/informativa" target="_blank">Informativa</a>.</p>
+                <button id="accept-cookies">Accetta</button> 
+                <button id="reject-cookies">Continua senza</button>
+            </div>`;
+        document.body.appendChild(cookieBanner);
 
-    const acceptBtn = document.getElementById("accept-cookies");
-    const rejectBtn = document.getElementById("rejected-cookies");
-    if (!localStorage.getItem("cookiesAccepted") && !localStorage.getItem("cookieRejected")) {
-        cookieBanner.style.display = "block";
+        const acceptBtn = document.getElementById("accept-cookies");
+        const rejectBtn = document.getElementById("reject-cookies");
+
+        if (acceptBtn && rejectBtn) {
+            acceptBtn.addEventListener("click", () => {
+                localStorage.setItem("cookiesAccepted", "true");
+                cookieBanner.style.display = "none";
+            });
+
+            rejectBtn.addEventListener("click", () => {
+                localStorage.setItem("cookiesRejected", "true");
+                cookieBanner.style.display = "none";
+            });
+        }
     }
-
-    acceptBtn.addEventListener("click", () => {
-        localStorage.setItem("cookiesAccepted", "true");
-        cookieBanner.style.display = "none";
-    });
-
-    rejectBtn.addEventListener("click", () => {
-        localStorage.setItem("cookiesRejected", "true");
-        cookieBanner.style.display = "none";
-    });
 });
